967cd5957ed541d42d3f9e1e721f0cfcfc7b8b88
indetronable <mohamed.kiouaz@gmail.com>
2017-06-15 11:53:34 +0100

    Null vector

--- main.R
+++ main.R
@@ -15,8 +15,6 @@ for(f in filenames) {
   colnames(data_) = c("x", "y", "z")
   data_$time = 1:nrow(data_)
   
-  #d = data.matrix(data_)
-  
   average_filter = function(X) {
     avg_filter = matrix(0, 1, nrow(data_))
     avg_filter[1, 1:n] = 1/n
@@ -25,20 +23,25 @@ for(f in filenames) {
   
   data_avg = apply(data_, 2, average_filter)
   
-  data_avg = apply(data_avg, 2, function(X) { return(detrend(X, 'linear')) })
+  data_avg = apply(data_avg, 2, function(X) detrend(X, 'linear'))
   
-  data_peaks = apply(data_avg, 2, function(X) { return(findpeaks(X, minpeakdistance = 45)) })
+  data_null = abs(data_avg) < .03
+  data_null = data_null[, "x"] & data_null[, "y"] & data_null[, "z"]
+  data_null = which(data_null %in% TRUE)
+  
+  data_peaks = apply(data_avg, 2, function(X) findpeaks(X, minpeakdistance = 45))
   
   pdf(file = paste("output_", f, ".pdf", sep = ""), 15, 10)
-  par(mfrow = c(3, 1))
+  par(mfrow = c(3, 1), bg = "lightgray")
   for(i in c("x", "y", "z")) {
-    plot(data_$time, data_avg[, i], type="l", lwd = 2, col = "blue", axes = FALSE, ann = FALSE, ylim = c(-1.5, 1.5), panel.first = grid(NA, 6, lwd = 2, lty = "solid"))
-    axis(1, las = 1)
+    plot(data_$time, data_avg[, i], type="l", lwd = 2, col = "blue", axes = FALSE, ann = FALSE, ylim = c(-1.5, 1.5), panel.first = grid(col = "white", lty = "solid"))
+    axis(1, las = 1, at = seq(0, length(data_$time), by = 25))
     axis(2, las = 1)
     box()
     title(xlab = "Time", ylab = "Signal", main = paste("Axis", i))
     points(data_[i], type = "p", lwd = 1, col = "red")
     points(data_peaks[[i]][, 2], data_peaks[[i]][, 1], type = "p", lwd = 2, col = "black", pch = 0)
+    points(data_null, integer(length(data_null)), type = "p", col = "green", pch = 5)
     legend("topright", c("Raw Signal", paste("Averaged and detrended Signal", n), "Peaks"), col = c("red","blue", "black"), pch = c(1, NA, 0), lty = c(0, 1, 0), lwd = c(2, 1, 2), bg = "white");
   }
   dev.off() 
