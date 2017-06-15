d38517bebf7069bda9bc42f9dc5d1ea3ad30f8e4
indetronable <mohamed.kiouaz@gmail.com>
2017-06-15 10:43:51 +0100

    Peaks

--- main.R
+++ main.R
@@ -2,13 +2,12 @@ rm(list = ls())
 
 ##### Variables #####
 
-n = 10
+n = 25
 filenames = c("1.xlsx", "2.xlsx", "3.xlsx", "4.xlsx", "5.xlsx")
 
 #####################
 
 library(xlsx)
-library(peaks)
 library(pracma)
 
 for(f in filenames) {
@@ -28,17 +27,19 @@ for(f in filenames) {
   
   data_avg = apply(data_avg, 2, function(X) { return(detrend(X, 'linear')) })
   
+  data_peaks = apply(data_avg, 2, function(X) { return(findpeaks(X, minpeakdistance = 45)) })
+  
   pdf(file = paste("output_", f, ".pdf", sep = ""), 15, 10)
   par(mfrow = c(3, 1))
   for(i in c("x", "y", "z")) {
-    plot(data_$time, data_avg[, i], type="l", lwd = 2, col = "blue", axes = FALSE, ann = FALSE, ylim = c(-1.5, 1.5))
-    grid(NA, 6, lwd = 2, lty = "solid")
+    plot(data_$time, data_avg[, i], type="l", lwd = 2, col = "blue", axes = FALSE, ann = FALSE, ylim = c(-1.5, 1.5), panel.first = grid(NA, 6, lwd = 2, lty = "solid"))
     axis(1, las = 1)
     axis(2, las = 1)
     box()
     title(xlab = "Time", ylab = "Signal", main = paste("Axis", i))
     points(data_[i], type = "p", lwd = 1, col = "red")
-    legend("topright", c("Raw Signal", paste("Averaged and detrended Signal", n)), col = c("red","blue"), pch = c(1, NA), lty = c(0, 1), lwd = 2);
+    points(data_peaks[[i]][, 2], data_peaks[[i]][, 1], type = "p", lwd = 2, col = "black", pch = 0)
+    legend("topright", c("Raw Signal", paste("Averaged and detrended Signal", n), "Peaks"), col = c("red","blue", "black"), pch = c(1, NA, 0), lty = c(0, 1, 0), lwd = c(2, 1, 2), bg = "white");
   }
   dev.off() 
   print(f)
