9335b56871e906cd8d75636dca603d2748c648a2
indetronable <mohamed.kiouaz@gmail.com>
2017-06-23 13:47:10 +0100

    Added norm of raw and everaged vectors

--- process.R
+++ process.R
@@ -6,11 +6,17 @@ ProcessData = function(data_) {
   avg_filter = matrix(0, 1, length(data_$time))
   avg_filter[1, 1:n] = 1/n
   
-  data_$avg = apply(data_$raw, 2, function(X) convolve(X, avg_filter))
-  colnames(data_$avg) = c("x", "y", "z")
-  data_$avg = apply(data_$avg, 2, function(X) detrend(X, 'linear'))
+  A = apply(data_$raw, 1, function(X) norm(as.matrix(X)))
+  data_$raw = cbind(data_$raw, A)
+  colnames(data_$raw) = c("x", "y", "z", "norm")
   
-  data_$null = apply(data_$avg, 1, function(X) norm(as.matrix(X))<NullVectorNormThreshold[data_$exercice])
+  data_$avg = apply(data_$raw[1:3], 2, function(X) convolve(X, avg_filter))
+  data_$avg = apply(data_$avg, 2, function(X) detrend(X, 'linear'))
+  A = apply(data_$avg, 1, function(X) norm(as.matrix(X)))
+  data_$avg = cbind(data_$avg, A)
+  colnames(data_$avg) = c("x", "y", "z", "norm")
+  
+  data_$null = data_$avg[, "norm"] < NullVectorNormThreshold[1]
   data_$null = which(data_$null %in% TRUE)
   i = length(data_$null)
   while(i > 1) {
@@ -23,7 +29,7 @@ ProcessData = function(data_) {
   data_$period = median(tail(data_$null, length(data_$null) - 1) - head(data_$null, length(data_$null) - 1))
   print(paste("Period =", data_$period))
   
-  data_$pos = apply(data_$avg, 2, function(X) acc2pos(data_$time, X))
+  data_$pos = apply(data_$avg[, 1:3], 2, function(X) acc2pos(data_$time, X))
   
   #data_$peaks = apply(data_$avg, 2, function(X) findpeaks(X, minpeakdistance = 45))
   
