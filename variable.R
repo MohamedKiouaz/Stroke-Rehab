b66563088a107c602534597df066a0c9c0bb8291
indetronable <mohamed.kiouaz@gmail.com>
2017-06-29 16:40:08 +0100

    Add personnal exercice

--- variable.R
+++ variable.R
@@ -2,8 +2,8 @@
 n = 20
 
 # Files to analyze
-filenames = c("1.xlsx", "2.xlsx", "3.xlsx", "4.xlsx", "5.xlsx")
-extype = c(1, 2, 3, 4, 5)
+filenames = c("1.xlsx", "2.xlsx", "3.xlsx", "4.xlsx", "5.xlsx", "Ex1_Me.csv")
+extype = c(1, 2, 3, 4, 5, 1)
 
 # Null Vector Threshold for every exercice
 NullVectorNormThreshold = c(.15, .18, .15, .5, .05)
