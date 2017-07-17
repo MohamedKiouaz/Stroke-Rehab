bc225599a04c4a702d7c8b0937e1669686c6f876
MohamedKiouaz <mohamed.kiouaz@gmail.com>
2017-07-17 13:50:24 +0100

    More data

--- src/variable.R
+++ src/variable.R
@@ -4,5 +4,5 @@ n = 40
 # Files to analyze
 
 data_folder = ("data")
-filenames = c("ex1.csv", "ex3.csv", "ex5.csv")
-extype = c(1, 3, 5)
+filenames = c("ex1.csv", "ex1_bis.csv", "ex3.csv", "ex5.csv")
+extype = c(1, 1, 3, 5)
