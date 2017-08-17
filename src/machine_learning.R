6546f6b80ee939babb0c1622813abc09c23c20af
MohamedKiouaz <mohamed.kiouaz@gmail.com>
2017-08-17 10:16:01 +0100

    More features

--- src/machine_learning.R
+++ src/machine_learning.R
@@ -2,37 +2,41 @@ library(caret)
 library(randomForest)
 
 feature_matrix = function(data_) {
-	f_matrix = matrix(0, data_$count, 5)
+	f_matrix = matrix(0, data_$count, 7)
 	for (i in 1:data_$count) {
 		if (mod(i, 4) != 0) {
 			period = GetRawPeriod(data_, i, col = 1:4)
 			time = GetTimePeriod(data_, i)
-			max_vector = period[which.max(period[, 4]), 1:3]
-			max_vector = as.matrix(unname(max_vector / sqrt(sum(max_vector ^ 2))))
+			max_vector = as.matrix(period[which.max(period[, 4]), 1:3])
+			max_vector = max_vector / norm(max_vector, t = "F")
 			f_matrix[i, 1:3] = max_vector
-			f_matrix[i, 4] = distance(time, period)
+			f_matrix[i, 4] = distance(time, period) / 9.81
 			f_matrix[i, 5] = data_$extype
+			f_matrix[i, 6] = (max(period[, 2]) - min(period[, 2])) / (max(period[, 1]) - min(period[, 1]))
+			f_matrix[i, 7] = (max(period[, 3]) - min(period[, 3])) / (max(period[, 1]) - min(period[, 1]))
 		}
 	}
 	
-	f_matrix[!(f_matrix[, 4] == 3), ]
+	f_matrix[f_matrix[, 5] != 0,]
 }
 
 test_matrix = function(data_) {
-	f_matrix = matrix(0, data_$count, 4)
+	f_matrix = matrix(0, data_$count, 7)
 	for (i in 1:data_$count) {
 		if (mod(i, 4) == 0) {
 			period = GetRawPeriod(data_, i, col = 1:4)
 			time = GetTimePeriod(data_, i)
-			max_vector = period[which.max(period[, 4]), 1:3]
-			max_vector = as.matrix(unname(max_vector / sqrt(sum(max_vector ^ 2))))
+			max_vector = as.matrix(period[which.max(period[, 4]), 1:3])
+			max_vector = max_vector / norm(max_vector, t = "F")
 			f_matrix[i, 1:3] = max_vector
-			f_matrix[i, 4] = distance(time, period)
+			f_matrix[i, 4] = distance(time, period) / 9.81
 			f_matrix[i, 5] = data_$extype
+			f_matrix[i, 6] = (max(period[, 2]) - min(period[, 2])) / (max(period[, 1]) - min(period[, 1]))
+			f_matrix[i, 7] = (max(period[, 3]) - min(period[, 3])) / (max(period[, 1]) - min(period[, 1]))
 		}
 	}
 	
-	f_matrix[!(f_matrix[, 4] == 3), ]
+	f_matrix[f_matrix[, 5] != 0,]
 }
 
 features = sapply(dd, feature_matrix)
@@ -41,14 +45,27 @@ test_matrix = sapply(dd, test_matrix)
 features = do.call(rbind, features)
 test_matrix = do.call(rbind, test_matrix)
 
-training_data = data.frame(vec = features[, 1:4], exercice = factor(features[, 5]))
-test_data = data.frame(vec = test_matrix[, 1:4], exercice = factor(test_matrix[, 5]))
+training_data = data.frame(vec = features[, 1:4],
+						   exercice = factor(features[, 5]),
+						   vec2 = features[, 6:7])
+test_data = data.frame(vec = test_matrix[, 1:4],
+					   exercice = factor(test_matrix[, 5]),
+					   vec2 = test_matrix[, 6:7])
 
-model.d_lm = train(exercice ~ ., data = training_data, method = 'rf')
+model.d_lm = train(exercice ~ .,
+				   data = training_data,
+				   method = 'rf',
+				   ntree = 2000)
 
 a = predict(model.d_lm$finalModel, newdata = test_data)
 b = predict(model.d_lm$finalModel)
 
-print(cbind(predicted = a, reference = test_data$exercice, is_right = a == test_data$exercice))
+print(head(
+	cbind(
+		predicted = a,
+		reference = test_data$exercice,
+		is_right = a == test_data$exercice
+	)
+))
 
-print(sum(a == test_data$exercice)/length(a))
+print(model.d_lm$finalModel)
