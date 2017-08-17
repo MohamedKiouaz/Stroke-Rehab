35388cfc842148b765ba69bb3520c8d0f801fad7
MohamedKiouaz <mohamed.kiouaz@gmail.com>
2017-08-17 10:17:21 +0100

    Update

--- src/variable.R
+++ src/variable.R
@@ -21,3 +21,18 @@ informations = data.frame(
 	reps = c(40, 56, 1, 1, 1, 1, 3, 5),
 	stringsAsFactors = FALSE
 )
+
+informations = data.frame(
+	folder = DATA_FOLDER,
+	filename = c(
+		"AO_ex1.csv",
+		"AO_ex3.csv",
+		"LC_ex1.csv",
+		"LC_ex3.csv",
+		"SG_ex1.csv",
+		"SG_ex3.csv"
+	),
+	extype = c(1, 3, 1, 3, 1, 3),
+	reps = c(35, 35, 35, 35, 35, 35),
+	stringsAsFactors = FALSE
+)
